21228d7c522dee1899bdce6b072bb593da1ad61a
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2020-11-11 11:37:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 80
+ }, 1000, 'easeOutBack');
+
+ event.preventDefault();
+  
+
+
+});
+
+
+
+}
+);
